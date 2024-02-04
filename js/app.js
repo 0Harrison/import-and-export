2adfb214d1e235a4ed8b418927f2640346a3e235
Harry <152967791+0Harrison@users.noreply.github.com>
2024-02-04 23:18:46 +0100

    EXPORT AND IMPORT

---
+++ js/app.js
@@ -0,0 +1,18 @@
+export default function school(){
+    console.log('UNIZIK')
+}
+
+
+export function num(x){
+    return x + 7;
+}
+
+export const who = "Harry";
+
+export let fruits = ['apple','banana','grape']
+
+export const myDetails = {
+    name: 'young',
+    age: 23,
+    town: "awka" 
+}
