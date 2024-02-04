1d3c1049fb0d82a5cdc0f83354adb1df96dbb40d
Harry <152967791+0Harrison@users.noreply.github.com>
2024-02-04 23:18:35 +0100

    EXPORT AND IMPORT

---
+++ js/main.js
@@ -0,0 +1,25 @@
+// default export
+import {default as whereToStudy} from './app.js'
+whereToStudy()
+
+// named export
+import {num , who, fruits, myDetails} from './app.js';
+console.log(num(5),who,fruits,myDetails)
+
+
+
+//OR I CAN DO IT THIS WAY
+
+
+
+// import {num} from './app.js';
+// console.log(num(5))
+
+// import {who} from './app.js';
+// console.log(who)
+
+// import {fruits} from './app.js';
+// console.log(fruits)
+
+// import {myDetails} from './app.js';
+// console.log(myDetails);
